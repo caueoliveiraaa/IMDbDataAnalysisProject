8e12da646021598a7a89f6d3bf9bd93e43d06d94
caueoliveiraaa <leo.siscobra@gmail.com>
2024-05-16 20:18:07 -0300

    Update main.py
    feat: learning about deploying dataframes.

--- website/main.py
+++ website/main.py
@@ -4,14 +4,26 @@ import numpy as np
 
 
 class Main:
-    def __init__():
+    def __init__(): 
         pass
 
     
     def run_website():
         st.title('Testing website')
+        
+        st.write('DataFrame')
+        df = pd.DataFrame({ 'column 1': [0, 1, 3], 'column 2': [4, 5, 6]})
+        
+        st.write(df)
+        st.write('Static table')
+        st.table(df)
 
+        st.write('Simple numpy dataframe')
+        dataframe = np.random.randn(10, 30)
+        st.dataframe(dataframe)
+        
+        
+        
 
 if __name__ == '__main__':
     Main.run_website()
-    
